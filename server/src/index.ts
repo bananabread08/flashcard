91c0073dbde504dba80438651b968fa5c08815b0
bananabread08 <rmkfermin@gmail.com>
2022-11-28 16:21:44 +0800

    add delete requestfor decks

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,12 +15,23 @@ app.get('/', (req: Request, res: Response) => {
   res.send('HOME');
 });
 
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
+});
+
 app.post('/decks', async (req: Request, res: Response) => {
   const deck = new Deck({
     title: req.body.title,
   });
   const createDeck = await deck.save();
   res.json(createDeck);
+});
+
+app.delete('/decks/:deckId', async (req: Request, res: Response) => {
+  const deckId = req.params.deckId;
+  const deck = await Deck.findByIdAndDelete(deckId);
+  res.json(deck);
 });
 
 // MongoDB
