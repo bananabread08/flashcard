e06bcbd48d293b5b684ceee206b3045542725577
bananabread08 <rmkfermin@gmail.com>
2022-11-28 15:17:43 +0800

    create form that handles post request from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import './App.css';
+import React, { useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState('');
 
+  async function handleCreateDeck(e: React.FormEvent) {
+    e.preventDefault();
+    await fetch('http://localhost:5001/decks', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setTitle('');
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Create Deck</label>
+        <input
+          type="text"
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
