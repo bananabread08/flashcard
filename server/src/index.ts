def8da132f7833a99caee3b0c11f2ed16efe3468
bananabread08 <rmkfermin@gmail.com>
2022-11-30 12:10:32 +0800

    import decksRouter and use this route as home

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,32 +7,11 @@ import Deck from './models/Deck';
 dotenv.config();
 const app = express();
 const PORT = 5001;
-
+import { decksRouter } from './routes/decksRoute';
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('HOME');
-});
-
-app.get('/decks', async (req: Request, res: Response) => {
-  const decks = await Deck.find();
-  res.json(decks);
-});
-
-app.post('/decks', async (req: Request, res: Response) => {
-  const deck = new Deck({
-    title: req.body.title,
-  });
-  const createDeck = await deck.save();
-  res.json(createDeck);
-});
-
-app.delete('/decks/:deckId', async (req: Request, res: Response) => {
-  const deckId = req.params.deckId;
-  const deck = await Deck.findByIdAndDelete(deckId);
-  res.json(deck);
-});
+app.use('/', decksRouter);
 
 // MongoDB
 mongoose.connect(process.env.MONGO_URI!).then(() => {
