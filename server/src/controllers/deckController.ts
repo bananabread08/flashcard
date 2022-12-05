0fc3ae9694a5d9bea0ad13f5bab4f00172b96051
bananabread08 <rmkfermin@gmail.com>
2022-12-05 10:52:06 +0800

    add functions for creating cards in deck, get current deck, and delete a card from current deck

--- server/src/controllers/deckController.ts
+++ server/src/controllers/deckController.ts
@@ -19,3 +19,29 @@ export async function deleteDeck(req: Request, res: Response) {
   const deck = await Deck.findByIdAndDelete(deckId);
   res.json(deck);
 }
+
+export async function createCardForDeck(req: Request, res: Response) {
+  const deckId = req.params.deckId;
+  const deck = await Deck.findById(deckId);
+  const { text } = req.body;
+  if (!deck) return res.status(400).send('No deck of this id exists');
+  deck.cards.push(text);
+  await deck.save();
+  res.json(deck);
+}
+
+export async function getCurrentDeck(req: Request, res: Response) {
+  const { deckId } = req.params;
+  const deck = await Deck.findById(deckId);
+  res.json(deck);
+}
+
+export async function deleteCard(req: Request, res: Response) {
+  const deckId = req.params.deckId;
+  const index = req.params.index;
+  const deck = await Deck.findById(deckId);
+  if (!deck) return res.status(400).send('No deck of this id exists');
+  deck.cards.splice(parseInt(index), 1);
+  await deck.save();
+  res.json(deck);
+}
