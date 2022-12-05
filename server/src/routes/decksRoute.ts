68913900b75566e4667cd7c7a939071761953dbb
bananabread08 <rmkfermin@gmail.com>
2022-12-05 10:53:06 +0800

    create routes for deleting cards, getting currentDeck, and creating cards

--- server/src/routes/decksRoute.ts
+++ server/src/routes/decksRoute.ts
@@ -1,16 +1,20 @@
 import express from 'express';
 import {
+  createCardForDeck,
   createDeck,
+  deleteCard,
   deleteDeck,
   getAllDecks,
+  getCurrentDeck,
 } from '../controllers/deckController';
 
 const router = express.Router();
 
 router.get('/decks', getAllDecks);
-
 router.post('/decks', createDeck);
-
 router.delete('/decks/:deckId', deleteDeck);
+router.get('/decks/:deckId', getCurrentDeck);
+router.post('/decks/:deckId/cards', createCardForDeck);
+router.delete('/decks/:deckId/cards/:index', deleteCard);
 
 export { router as decksRouter };
